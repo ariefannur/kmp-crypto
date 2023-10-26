913c479dfb6520ee3d1408006ee0bd3968c68732
Arief maffrudin <arief.platk@gmail.com>
2023-10-26 14:42:42 +0700

    update iOS client

--- shared/src/commonMain/kotlin/com/github/ariefannur/kmm/crypto/domain/base/DataState.kt
+++ shared/src/commonMain/kotlin/com/github/ariefannur/kmm/crypto/domain/base/DataState.kt
@@ -5,3 +5,10 @@ sealed class DataState<out T: Any>{
     data class Success<T: Any>(val result: T) : DataState<T>()
     data class Failure(val errorCode: Int, val message: String) : DataState<Nothing>()
 }
+
+data class DataStateNative<out T: Any>(val state: Int, val result: T? = null, val error: ErrorNative? = null)
+data class ErrorNative(val code: Int, val message: String)
+
+const val NATIVE_LOADING = 1
+const val NATIVE_SUCCESS = 2
+const val NATIVE_ERROR = 3
--- shared/src/commonMain/kotlin/com/github/ariefannur/kmm/crypto/domain/base/UseCase.kt
+++ shared/src/commonMain/kotlin/com/github/ariefannur/kmm/crypto/domain/base/UseCase.kt
@@ -1,5 +1,6 @@
 package com.github.ariefannur.kmm.crypto.domain.base
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutineScope
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 
@@ -20,11 +21,27 @@ abstract class UseCase<out Type, in Params> where Type : Any {
                     callback.invoke(DataState.Success(it))
                 }
             } catch (e: Exception) {
-                println("Error ${e.message}")
                 callback.invoke(DataState.Failure(404, e.message ?: ""))
             }
         }
     }
 
     class None
+
+    @NativeCoroutineScope
+    val coroutineScope: CoroutineScope = MainScope()
+
+    fun runNative(params: Params, callback: (DataStateNative<Type>) -> Unit = {}) {
+        coroutineScope.launch(Dispatchers.Default) {
+            try {
+                callback.invoke(DataStateNative(NATIVE_LOADING))
+                val deferred = async {  run(params) }
+                deferred.await().collect {
+                    callback.invoke(DataStateNative(NATIVE_SUCCESS, it))
+                }
+            } catch (e: Exception) {
+                callback.invoke(DataStateNative( state = NATIVE_ERROR, error = ErrorNative(404, e.message ?: "")))
+            }
+        }
+    }
 }
